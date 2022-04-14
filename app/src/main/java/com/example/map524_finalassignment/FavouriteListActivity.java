04d9a34840082327c6673fa89de9aee7403a87da
Shivani-012 <63118364+Shivani-012@users.noreply.github.com>
2022-04-14 15:21:24 -0400

    Added User List Implementation (connection to database, recycler view, listeners).

--- app/src/main/java/com/example/map524_finalassignment/FavouriteListActivity.java
+++ app/src/main/java/com/example/map524_finalassignment/FavouriteListActivity.java
@@ -1,6 +1,5 @@
 package com.example.map524_finalassignment;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,19 +7,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements
+public class FavouriteListActivity extends AppCompatActivity implements
         MovieRecyclerAdapter.OnSelectListener,
-        NetworkingService.NetworkingListener {
+        NetworkingService.NetworkingListener{
 
     ArrayList<Movie> movieList = new ArrayList<Movie>();
     MovieRecyclerAdapter movieAdapter;
     RecyclerView movieTable;
+
+    DatabaseManager dbManager;
 
     NetworkingService networkingManager;
     JsonService jsonManager;
@@ -28,7 +26,12 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favourite_list);
+
+        dbManager = ((MyApp)getApplication()).dbManager;
+        dbManager.getMovieDB(this);
+
+        dbManager.getFavouriteMovies();
 
         networkingManager = ((MyApp)getApplication()).getNetworkingService();
         jsonManager = ((MyApp)getApplication()).getJsonService();
@@ -36,40 +39,11 @@ public class MainActivity extends AppCompatActivity implements
         networkingManager.listener = this;
         networkingManager.getTrendingMovies();
 
-        movieTable = findViewById(R.id.recyclerViewMain);
+        movieTable = findViewById(R.id.recyclerViewFavourite);
 
         movieAdapter = new MovieRecyclerAdapter(movieList, this, this, networkingManager);
         movieTable.setAdapter(movieAdapter);
         movieTable.setLayoutManager((new LinearLayoutManager(this)));
-    }
-
-    // creator for Menu that inflates it with items
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    // function that detects when a menu item is selected
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        switch (item.getItemId()){
-            // if search is selected
-            case R.id.search_for_movie:
-                // open search activity
-                Intent searchIntent = new Intent(this, SearchActivity.class);
-                startActivity(searchIntent);
-                break;
-            // if "my lists" is selected
-            case R.id.view_user_lists:
-                // open UserList activity
-                Intent userListIntent = new Intent(this, FavouriteListActivity.class);
-                startActivity(userListIntent);
-                break;
-        }
-
-        return true;
     }
 
     @Override
@@ -85,14 +59,12 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void dataListener(String jsonString) {
-        movieList = jsonManager.getMoviesFromJSON(jsonString);
-        movieAdapter = new MovieRecyclerAdapter(movieList, this, this, networkingManager);
-        movieTable.setAdapter(movieAdapter);
-        movieAdapter.notifyDataSetChanged();
+
     }
 
     @Override
     public void imageListener(Bitmap image, MovieRecyclerAdapter.MovieViewHolder holder) {
         holder.posterImg.setImageBitmap(image);
     }
+
 }
